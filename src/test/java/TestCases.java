6db22ebf225876a808d24cf2768a2d8fdcfcf7d8
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-11-18 22:51:03 -0600

    added test2

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -6,4 +6,10 @@ public class TestCases {
     void test1(){
         System.out.println("I am in site test1");
     }
+
+
+    @Test
+    void test2(){
+        System.out.println("I am in site test2");
+    }
 }
