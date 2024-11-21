1c7a62482984a86e231963edf89a719507106889
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-11-21 02:19:15 -0600

    pr3

--- src/test/java/AlexLeo/BaseTest.java
+++ src/test/java/AlexLeo/BaseTest.java
@@ -1,32 +1,64 @@
 package AlexLeo;
-
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeMethod;
+//
+//import org.testng.annotations.BeforeSuite;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.Properties;
+//
+//import java.time.Duration;
+//
+//
+//public class BaseTest {
+//
+//    WebDriver driver;
+//
+//    @BeforeMethod
+//    public void setUp() {
+//        driver = new ChromeDriver();
+//        driver.get("https://askomdch.com/");
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+//    }
+//
+//    @AfterMethod
+//    public void tearDown() {
+//        driver.quit();
+//    }
+//}
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import org.testng.annotations.BeforeSuite;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import java.time.Duration;
-
 public class BaseTest {
-
-    WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("https://askomdch.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-debugging-port=9222");
+
+        driver = new ChromeDriver(options);
+        driver.get("https://askomdch.com");
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
