3a73896191ac0cdab16cedacfec4889ccf020fc9
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-11-22 00:52:50 -0600

    pr6

--- src/test/java/AlexLeo/BaseTest.java
+++ src/test/java/AlexLeo/BaseTest.java
@@ -1,34 +1,66 @@
 package AlexLeo;
-//
+////
+////import org.openqa.selenium.WebDriver;
+////import org.openqa.selenium.chrome.ChromeDriver;
+////import org.testng.annotations.AfterMethod;
+////import org.testng.annotations.BeforeMethod;
+////
+////import org.testng.annotations.BeforeSuite;
+////
+////import java.io.IOException;
+////import java.io.InputStream;
+////import java.util.Properties;
+////
+////import java.time.Duration;
+////
+////
+////public class BaseTest {
+////
+////    WebDriver driver;
+////
+////    @BeforeMethod
+////    public void setUp() {
+////        driver = new ChromeDriver();
+////        driver.get("https://askomdch.com/");
+////        driver.manage().window().maximize();
+////        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+////    }
+////
+////    @AfterMethod
+////    public void tearDown() {
+////        driver.quit();
+////    }
+////}
 //import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
 //import org.testng.annotations.AfterMethod;
 //import org.testng.annotations.BeforeMethod;
 //
-//import org.testng.annotations.BeforeSuite;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.util.Properties;
-//
-//import java.time.Duration;
-//
-//
 //public class BaseTest {
-//
 //    WebDriver driver;
 //
 //    @BeforeMethod
 //    public void setUp() {
-//        driver = new ChromeDriver();
-//        driver.get("https://askomdch.com/");
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+//        System.setProperty("webdriver.chrome.driver",
+//                "C:Program Files\\Google\\Chrome\\Application\\chromedriver\\chromedriver.exe");
+//
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("--disable-gpu");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--remote-debugging-port=9222");
+//
+//        driver = new ChromeDriver(options);
+//        driver.get("https://askomdch.com");
 //    }
 //
 //    @AfterMethod
 //    public void tearDown() {
-//        driver.quit();
+//        if (driver != null) {
+//            driver.quit();
+//        }
 //    }
 //}
 import org.openqa.selenium.WebDriver;
@@ -38,12 +70,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-    protected WebDriver driver;
+    WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application\\chromedriver");
+        // Убедитесь, что путь к chromedriver правильный и использует двойные обратные слэши для Windows
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe");
 
+        // Настройка опций Chrome для headless-режима
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
@@ -51,14 +85,18 @@ public class BaseTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-debugging-port=9222");
 
+        // Инициализация драйвера с опциями
         driver = new ChromeDriver(options);
         driver.get("https://askomdch.com");
     }
 
     @AfterMethod
     public void tearDown() {
+        // Закрытие драйвера после выполнения тестов
         if (driver != null) {
             driver.quit();
         }
     }
 }
+
+
